433b38c1d8402c6c00875f73495ec37a02c6e683
Pythux <pythux@nowhere.org>
2021-04-23 23:00:26 +0200

    indent

--- front/src/components/HomePage.vue
+++ front/src/components/HomePage.vue
@@ -1,23 +1,23 @@
 <template>
-  <v-container>
-    <div class="codeZone">
-      <textarea id="from-source" name="from-source" style="background-color: rgb(180, 255, 251);"></textarea>
-    </div>
-    <div class="complieBtn d-flex flex-column justify-space-around">
-      <Intro />
-      <v-btn
-          color="cyan"
-          dark
-          elevation="2"
-          large
-          :loading="loadingCompiling"
+    <v-container>
+        <div class="codeZone">
+        <textarea id="from-source" name="from-source" style="background-color: rgb(180, 255, 251);"></textarea>
+        </div>
+        <div class="complieBtn d-flex flex-column justify-space-around">
+        <Intro />
+        <v-btn
+            color="cyan"
+            dark
+            elevation="2"
+            large
+            :loading="loadingCompiling"
         >>> Compile >></v-btn>
-      <ExempleCode v-on:choice="changeChoiceCode" />
-    </div>
-    <div class="compileZone">
-      <textarea id="to-asm" name="to-asm"></textarea>
-    </div>
-  </v-container>
+        <ExempleCode v-on:choice="changeChoiceCode" />
+        </div>
+        <div class="compileZone">
+        <textarea id="to-asm" name="to-asm"></textarea>
+        </div>
+    </v-container>
 </template>
 
 
@@ -26,67 +26,67 @@
     import Intro from './Intro.vue'
     import ExempleCode from './ExempleCode.vue'
 
-  export default Vue.extend({
-    data: () => ({
-        loadingCompiling: false,
-        choiceCode: null,
-      }
-    ),
-    components: {
-      Intro,
-      ExempleCode,
-    },
-    methods: {
-      changeChoiceCode(choice) {
-        console.log(choice)
-      }
-    }
-  })
+    export default Vue.extend({
+        data: () => ({
+                loadingCompiling: false,
+                choiceCode: null,
+            }
+        ),
+        components: {
+            Intro,
+            ExempleCode,
+        },
+        methods: {
+            changeChoiceCode(choice: string) {
+                console.log(choice)
+            }
+        }
+    })
 </script>
 
 
 <style>
 .container {
-  height: 100%;
+    height: 100%;
 }
 
 .container {
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 10px;
-  grid-auto-rows: minmax(100px, auto);
-  grid-auto-columns: minmax(100px, auto);
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 10px;
+    grid-auto-rows: minmax(100px, auto);
+    grid-auto-columns: minmax(100px, auto);
 }
 
 textarea {
-  resize: none !important;
-  height: 80vh;
-  flex-grow: 1;
-  padding: 20px;
-  margin: 10px;
+    resize: none !important;
+    height: 80vh;
+    flex-grow: 1;
+    padding: 20px;
+    margin: 10px;
 }
 
 .codeZone {
-  grid-row: 1;
-  grid-column: 1;
-  display: flex;
-  flex-direction: column;
+    grid-row: 1;
+    grid-column: 1;
+    display: flex;
+    flex-direction: column;
 }
 
 .complieBtn {
-  grid-row: 1;
-  grid-column: 2;
-  /* background-color: rgb(147, 242, 255); */
+    grid-row: 1;
+    grid-column: 2;
+    /* background-color: rgb(147, 242, 255); */
 }
 
 .v-btn__content {
-  text-transform: none;
+    text-transform: none;
 }
 
 .compileZone {
-  grid-row: 1;
-  grid-column: 3;
-  display: flex;
-  flex-direction: column;
+    grid-row: 1;
+    grid-column: 3;
+    display: flex;
+    flex-direction: column;
 }
 </style>
--- front/src/components/Intro.vue
+++ front/src/components/Intro.vue
@@ -13,65 +13,65 @@
 <script>
 
 function setupTypewriter(t) {
-  var HTML = t.innerHTML;
+    var HTML = t.innerHTML;
 
-  t.innerHTML = "";
+    t.innerHTML = "";
 
-  var cursorPosition = 0,
+    var cursorPosition = 0,
     tag = "",
     writingTag = false,
     tagOpen = false,
     typeSpeed = 10,
     tempTypeSpeed = 0;
 
-  var type = function () {
-    if (writingTag === true) {
-      tag += HTML[cursorPosition];
+    var type = function () {
+        if (writingTag === true) {
+            tag += HTML[cursorPosition];
+        }
+
+        if (HTML[cursorPosition] === "<") {
+            tempTypeSpeed = 0;
+            if (tagOpen) {
+                tagOpen = false;
+                writingTag = true;
+            } else {
+                tag = "";
+                tagOpen = true;
+                writingTag = true;
+                tag += HTML[cursorPosition];
+            }
+        }
+        if (!writingTag && tagOpen) {
+            tag.innerHTML += HTML[cursorPosition];
+        }
+        if (!writingTag && !tagOpen) {
+            if (HTML[cursorPosition] === " ") {
+                tempTypeSpeed = 0;
+            } else {
+                tempTypeSpeed = Math.random() * typeSpeed + 50;
+            }
+            t.innerHTML += HTML[cursorPosition];
+        }
+        if (writingTag === true && HTML[cursorPosition] === ">") {
+            tempTypeSpeed = Math.random() * typeSpeed + 50;
+            writingTag = false;
+            if (tagOpen) {
+                var newSpan = document.createElement("span");
+                t.appendChild(newSpan);
+                newSpan.innerHTML = tag;
+                tag = newSpan.firstChild;
+            }
+        }
+
+        cursorPosition += 1;
+        if (cursorPosition < HTML.length - 1) {
+            setTimeout(type, tempTypeSpeed);
+        }
     }
 
-    if (HTML[cursorPosition] === "<") {
-      tempTypeSpeed = 0;
-      if (tagOpen) {
-        tagOpen = false;
-        writingTag = true;
-      } else {
-        tag = "";
-        tagOpen = true;
-        writingTag = true;
-        tag += HTML[cursorPosition];
-      }
+    return {
+        type: type,
     }
-    if (!writingTag && tagOpen) {
-      tag.innerHTML += HTML[cursorPosition];
-    }
-    if (!writingTag && !tagOpen) {
-      if (HTML[cursorPosition] === " ") {
-        tempTypeSpeed = 0;
-      } else {
-        tempTypeSpeed = Math.random() * typeSpeed + 50;
-      }
-      t.innerHTML += HTML[cursorPosition];
-    }
-    if (writingTag === true && HTML[cursorPosition] === ">") {
-      tempTypeSpeed = Math.random() * typeSpeed + 50;
-      writingTag = false;
-      if (tagOpen) {
-        var newSpan = document.createElement("span");
-        t.appendChild(newSpan);
-        newSpan.innerHTML = tag;
-        tag = newSpan.firstChild;
-      }
-    }
-
-    cursorPosition += 1;
-    if (cursorPosition < HTML.length - 1) {
-      setTimeout(type, tempTypeSpeed);
-    }
-  };
-
-  return {
-    type: type,
-  };
 }
 
 export default {
@@ -85,47 +85,46 @@ export default {
 
 <style lang="scss" scoped>
 .var-highlight {
-  color: rgb(138, 92, 8);
+    color: rgb(138, 92, 8);
 }
 .string-highlight {
-  color: red;
+    color: red;
 }
 
 #presentation-typewriter {
-  font-size: 1em;
-  font-family: "Courier New";
-//   width: 30%;
+    font-size: 1em;
+    font-family: "Courier New";
 }
 
 #presentation-typewriter:after {
-  content: "|";
-  animation: blink 500ms linear infinite alternate;
+    content: "|";
+    animation: blink 500ms linear infinite alternate;
 }
 
 @-webkit-keyframes blink {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
 }
 
 @-moz-keyframes blink {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
 }
 
 @keyframes blink {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
 }
 </style>
