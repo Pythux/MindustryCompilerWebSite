8f986481dcfdf1abafeff837e940bfc7de948d1c
Pythux <pythux@nowhere.org>
2021-04-23 20:22:16 +0200

    up

---
+++ front/src/components/ExempleCode.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-btn-toggle
+        v-model="choice"
+        color="deep-purple accent-3"
+        group
+        class="d-flex flex-column"
+      >
+        <v-btn value="jumpRef">
+          Jump Ref
+        </v-btn>
+        <v-btn value="if">
+          If
+        </v-btn>
+
+        <v-btn value="fun">
+          Function
+        </v-btn>
+
+        <v-btn value="module">
+          Module
+        </v-btn>
+
+        <v-btn value="for">
+          For loop
+        </v-btn>
+      </v-btn-toggle>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            choice: null,
+        }
+    },
+    watch: {
+        choice() {
+            console.log(`emit: ${this.choice}`)
+            this.$emit('choice', this.choice)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- front/src/components/HomePage.vue
+++ front/src/components/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <div class="codeZone">
-      <textarea id="from-source" name="from-source"></textarea>
+      <textarea id="from-source" name="from-source" style="background-color: rgb(180, 255, 251);"></textarea>
     </div>
     <div class="complieBtn d-flex flex-column justify-space-around">
       <Intro />
@@ -11,7 +11,8 @@
           elevation="2"
           large
           :loading="loadingCompiling"
-        >Compile</v-btn>
+        >>> Compile >></v-btn>
+      <ExempleCode v-on:choice="changeChoiceCode" />
     </div>
     <div class="compileZone">
       <textarea id="to-asm" name="to-asm"></textarea>
@@ -21,71 +22,71 @@
 
 
 <script lang="ts">
-  import Vue from 'vue'
-  import Intro from './Intro.vue'
+    import Vue from 'vue'
+    import Intro from './Intro.vue'
+    import ExempleCode from './ExempleCode.vue'
 
   export default Vue.extend({
-    name: 'HelloWorld',
-
     data: () => ({
         loadingCompiling: false,
+        choiceCode: null,
       }
     ),
     components: {
       Intro,
+      ExempleCode,
+    },
+    methods: {
+      changeChoiceCode(choice) {
+        console.log(choice)
+      }
     }
   })
 </script>
 
 
 <style>
-  .container {
-    height: 100%;
-  }
+.container {
+  height: 100%;
+}
 
-  .container {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 10px;
-    grid-auto-rows: minmax(100px, auto);
-    grid-auto-columns: minmax(100px, auto);
-  }
+.container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+  grid-auto-rows: minmax(100px, auto);
+  grid-auto-columns: minmax(100px, auto);
+}
 
-  textarea {
-    resize: none !important;
-    height: 80vh;
-    flex-grow: 1;
-    padding: 20px;
-    margin: 10px;
-  }
+textarea {
+  resize: none !important;
+  height: 80vh;
+  flex-grow: 1;
+  padding: 20px;
+  margin: 10px;
+}
 
-  /* .title {
-    grid-row: 1;
-    grid-column: 1 / 4;
-    background-color: red;
-  } */
+.codeZone {
+  grid-row: 1;
+  grid-column: 1;
+  display: flex;
+  flex-direction: column;
+}
 
-  .codeZone {
-    grid-row: 1;
-    grid-column: 1;
+.complieBtn {
+  grid-row: 1;
+  grid-column: 2;
+  /* background-color: rgb(147, 242, 255); */
+}
 
-    background-color: teal;
-    display: flex;
-    flex-direction: column;
-  }
+.v-btn__content {
+  text-transform: none;
+}
 
-  .complieBtn {
-    grid-row: 1;
-    grid-column: 2;
-    background-color: rgb(147, 242, 255);
-  }
-
-  .compileZone {
-    grid-row: 1;
-    grid-column: 3;
-    background-color: gray;
-    display: flex;
-    flex-direction: column;
-  }
-
+.compileZone {
+  grid-row: 1;
+  grid-column: 3;
+  display: flex;
+  flex-direction: column;
+}
 </style>
